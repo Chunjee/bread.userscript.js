610aa90162e7df0eecf3c71f37f1c16532446f9f
Shawn Brooker <ngachunjee@gmail.com>
2022-05-24 13:53:47 -0700

    fix: userscript warnings

--- bread.js
+++ bread.js
@@ -45,68 +45,65 @@ GM_config.init(
 	});
 
 GM_registerMenuCommand('Configuration', () => {
-    GM_config.open()
+	GM_config.open();
 });
 
-minWordLength = GM_config.get('MinWordLength');
-minTextLength = GM_config.get('MinTextLength');
-boldRatio = GM_config.get('BoldRatio');
+var minWordLength = GM_config.get('MinWordLength');
+var minTextLength = GM_config.get('MinTextLength');
+var boldRatio = GM_config.get('BoldRatio');
 
 function insertTextBefore(text, node, bold) {
-    if (bold) {
-        var span = document.createElement("span");
-        span.style.fontWeight = "bolder";
-        span.appendChild(document.createTextNode(text));
+	if (bold) {
+		var span = document.createElement("span");
+		span.style.fontWeight = "bolder";
+		span.appendChild(document.createTextNode(text));
 
-        node.parentNode.insertBefore(span, node);
-    }
-    else {
-        node.parentNode.insertBefore(document.createTextNode(text), node);
-    }
+		node.parentNode.insertBefore(span, node);
+	} else {
+		node.parentNode.insertBefore(document.createTextNode(text), node);
+	}
 }
 
 function processNode(node) {
-    var walker = document.createTreeWalker(node, NodeFilter.SHOW_TEXT, {
-        acceptNode: (node) => {
-            return (
-                node.parentNode.nodeName !== 'SCRIPT' &&
-                node.parentNode.nodeName !== 'NOSCRIPT' &&
-                node.parentNode.nodeName !== 'STYLE' &&
-                node.nodeValue.length >= minTextLength) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT;
-        }
-    });
+	var walker = document.createTreeWalker(node, NodeFilter.SHOW_TEXT, {
+		acceptNode: (node) => {
+			return (
+				node.parentNode.nodeName !== 'SCRIPT' &&
+				node.parentNode.nodeName !== 'NOSCRIPT' &&
+				node.parentNode.nodeName !== 'STYLE' &&
+				node.nodeValue.length >= minTextLength) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT;
+		}
+	});
 
-    var node;
-    while (node = walker.nextNode()) {
-        var text = node.nodeValue;
-        var wStart = -1, wLen = 0, eng = false;
+	while (node = walker.nextNode()) {
+		var text = node.nodeValue;
+		var wStart = -1, wLen = 0, eng = false;
 
-        // English letters only
-        for (var i = 0; i <= text.length; i++) { // We use <= here because we want to include the last character in the loop
-            var cEng = i < text.length ? /[\p{Letter}\p{Mark}]/u.test(text[i]) : false;
+		// English letters only
+		for (var i = 0; i <= text.length; i++) { // We use <= here because we want to include the last character in the loop
+			var cEng = i < text.length ? /[\p{Letter}\p{Mark}]/u.test(text[i]) : false;
 
-            if (i == text.length || eng !== cEng) {
-                // State flipped or end of string
-                if (eng && wLen >= minWordLength) {
-                    var word = text.substring(wStart, wStart + wLen);
-                    var numBold = Math.ceil(word.length * boldRatio);
-                    var bt = word.substring(0, numBold), nt = word.substring(numBold);
-                    insertTextBefore(bt, node, true);
-                    insertTextBefore(nt, node, false);
-                } else if (wLen > 0) {
-                    var word = text.substring(wStart, wStart + wLen);
-                    insertTextBefore(word, node, false);
-                }
-                wStart = i;
-                wLen = 1;
-                eng = cEng;
-            } else {
-                wLen++;
-            }
-        }
-
-        node.nodeValue = ""; // Can't remove the node (otherwise the tree walker will break) so just set it to empty
-    }
+			if (i == text.length || eng !== cEng) {
+				// State flipped or end of string
+				if (eng && wLen >= minWordLength) {
+					var word = text.substring(wStart, wStart + wLen);
+					var numBold = Math.ceil(word.length * boldRatio);
+					var bt = word.substring(0, numBold), nt = word.substring(numBold);
+					insertTextBefore(bt, node, true);
+					insertTextBefore(nt, node, false);
+				} else if (wLen > 0) {
+					var word2 = text.substring(wStart, wStart + wLen);
+					insertTextBefore(word2, node, false);
+				}
+				wStart = i;
+				wLen = 1;
+				eng = cEng;
+			} else {
+				wLen++;
+			}
+		}
+		node.nodeValue = ""; // Can't remove the node (otherwise the tree walker will break) so just set it to empty
+	}
 }
 
 // This event listener fires when the page loads
